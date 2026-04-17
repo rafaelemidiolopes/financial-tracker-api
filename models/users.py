f9882280ffe99708148a97e8aa2556d891f6b797
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-04-16 21:10:26 -0300

    fix: fixed missed tablename and create relationship in User model

--- models/users.py
+++ models/users.py
@@ -1,9 +1,12 @@
 from database import Base
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String
 
 class User(Base):
+    __tablename__ = 'users'
+    
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    transactions: Mapped[list['Transaction']] = relationship('Transaction', back_populates='user')
     password_hash: Mapped[str] = mapped_column(String(300))
     is_active: Mapped[bool] = mapped_column(default=True)
