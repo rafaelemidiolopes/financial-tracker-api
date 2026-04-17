36d9c2509ef912c9f084abb69f0aaaaf056a8ba2
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-04-17 06:16:05 -0300

    feat: add user schemas

---
+++ schemas/users.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, EmailStr, ConfigDict
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+    
+class UserResponse(BaseModel):
+    id: int 
+    email: EmailStr
+    
+    model_config = ConfigDict(from_attributes = True)
+    
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str
+    
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+    
+class UpdateUser(BaseModel):
+    email: EmailStr | None = None
+    password: str | None = None
