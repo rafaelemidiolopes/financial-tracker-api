4f9f817bc46e48f31f474111699507b409068ad4
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-04-16 21:08:18 -0300

    feat: create Transaction table model

---
+++ models/transactions.py
@@ -0,0 +1,20 @@
+import enum
+from database import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Enum, Float, ForeignKey
+
+class Type(enum.Enum):
+    income = 'income'
+    expense = 'expense'
+
+class Transaction(Base):
+    __tablename__ = 'transactions'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+    user: Mapped['User'] = relationship('User', back_populates='transactions')
+    type: Mapped[Type] = mapped_column(Enum(Type), nullable=False)
+    amount: Mapped[float] = mapped_column(Float, nullable=False)
+    category: Mapped[str] = mapped_column(String(50), nullable=False)
+    description: Mapped[str] = mapped_column(String(100))
+    
