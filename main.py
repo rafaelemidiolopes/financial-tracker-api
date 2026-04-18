7ff0d1721f0a0cf1616d53a79cf32a04deba7387
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-04-18 16:55:57 -0300

    feat: add lifespan, setup and user router in main

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from fastapi import FastAPI
+from routers.users import router as users_router
+from models.users import User 
+from models.transactions import Transaction
+from database import Base, engine
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    
+    yield
+    
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(users_router)
