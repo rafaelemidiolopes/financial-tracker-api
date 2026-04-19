254a958b9152d3be7f2701f9efe5c3fa40a0979e
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-04-19 15:34:50 -0300

    feat: add get_current_user dependency for JWT authentication

--- core/security.py
+++ core/security.py
@@ -1,10 +1,16 @@
 from passlib.context import CryptContext
-from jwt import encode
+from jwt import encode, decode, ExpiredSignatureError, InvalidSignatureError, DecodeError
 from datetime import datetime, timedelta, timezone
 import os
 from dotenv import load_dotenv
+from models.users import User
+from fastapi import security, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import get_db
 
 load_dotenv()
+
+oauth2 = security.OAuth2PasswordBearer(tokenUrl='/user')
 
 SECRET_KEY = os.getenv('SECRET_KEY')
     
@@ -30,3 +36,23 @@ def create_access_token(user_data: dict):
     token = encode(payload, SECRET_KEY, algorithm = ALGORITHM)
     
     return token
+
+def get_current_user(db: Session = Depends(get_db), token = Depends(oauth2)):
+    try:
+        payload = decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        
+        user = db.query(User).filter_by(id = int(payload['sub'])).first()
+
+        if not user:
+            raise HTTPException(status_code=404, detail='User not found! ')
+    
+    except ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail='token has expired', headers={'WWW-Autheticate': 'Bearer'})
+    
+    except InvalidSignatureError:
+        raise HTTPException(status_code=401, detail='invalid signature token', headers={'WWW-Autheticate': 'Bearer'})
+    
+    except DecodeError:
+        raise HTTPException(status_code=401, detail='invalid decode error', headers={'WWW-Autheticate': 'Bearer'})
+    
+    return user
