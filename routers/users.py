ce35675cc722c1d26d8fed68c943e17640a691b4
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-04-22 21:59:31 -0300

    refactor: standardize user service calls

--- routers/users.py
+++ routers/users.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, Depends
 from schemas.users import UserCreate, UserResponse, TokenResponse, UserLogin, UpdateUser, UpdatePassword
 from database import get_db
 from sqlalchemy.orm import Session
-from services.users import create_user as create_user_service
 from services import users
 from core.security import get_current_user
 
@@ -10,7 +9,7 @@ router = APIRouter()
 
 @router.post('/users', response_model=UserResponse)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    return create_user_service(user, db)
+    return users.create_user(user, db)
 
 @router.post('/user', response_model=TokenResponse)
 def login_user(user_data: UserLogin, db: Session = Depends(get_db)):
