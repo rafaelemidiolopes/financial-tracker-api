ca3e6eefbf97d7d4baa8bbcbab96ae4c3c4ec1d5
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-04-25 16:10:56 -0300

    feat: include transaction router in main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routers.users import router as users_router
+from routers.transactions import router as transaction_router
 from models.users import User 
 from models.transactions import Transaction
 from database import Base, engine
@@ -13,4 +14,5 @@ async def lifespan(app: FastAPI):
     
 app = FastAPI(lifespan=lifespan)
 
+app.include_router(transaction_router)
 app.include_router(users_router)
