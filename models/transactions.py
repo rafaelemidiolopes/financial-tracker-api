8a87cbac2b13f5eb720c506176dc23985239bf02
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-04-26 21:27:05 -0300

    feat: add created_at field and remove redundancy in transactions models

--- models/transactions.py
+++ models/transactions.py
@@ -2,6 +2,7 @@ import enum
 from database import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, Enum, Float, ForeignKey
+from datetime import datetime, timezone
 
 class Type(enum.Enum):
     income = 'income'
@@ -11,10 +12,11 @@ class Transaction(Base):
     __tablename__ = 'transactions'
     
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
     user: Mapped['User'] = relationship('User', back_populates='transactions')
     type: Mapped[Type] = mapped_column(Enum(Type))
     amount: Mapped[float] = mapped_column(Float)
     category: Mapped[str] = mapped_column(String(50))
     description: Mapped[str] = mapped_column(String(100), nullable=True)
+    created_at: Mapped[datetime] = mapped_column(default = lambda: datetime.now(timezone.utc))
     
