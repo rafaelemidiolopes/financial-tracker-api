7c6d670e7df12e4c1a192ee13c0b4c417b1e5de3
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-05-09 17:46:36 -0300

    test: add login test for non-existent user

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,6 +1,6 @@
-from services.users import create_user
+from services.users import create_user, login_user
 from unittest.mock import MagicMock
-from schemas.users import UserCreate
+from schemas.users import UserCreate, UserLogin
 from fastapi import HTTPException
 import pytest
 from models.users import User
@@ -26,3 +26,13 @@ def test_email_exists():
     
     with pytest.raises(HTTPException):
         create_user(user, fake_db)
+        
+def test_user_dont_exists():
+    fake_db = MagicMock()
+    
+    fake_db.query.return_value.filter_by.return_value.first.return_value = None
+    
+    user = UserLogin(email = 'email@test.com', password = 'abc')
+    
+    with pytest.raises(HTTPException):
+        login_user(user, fake_db)
