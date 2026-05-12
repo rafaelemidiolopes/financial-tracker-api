1e0a3ed892be48e45985d6023796756682031e7f
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-05-12 06:50:03 -0300

    test: ensure get_transactions returns filtered results

--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -1,6 +1,6 @@
 from unittest.mock import MagicMock
-from services.transactions import create_transaction
-from schemas.transactions import TransactionCreate
+from services.transactions import create_transaction, get_transactions
+from schemas.transactions import TransactionCreate, TransactionFilters
 
 def test_success_create_transaction_case():
     fake_db = MagicMock()
@@ -18,3 +18,23 @@ def test_success_create_transaction_case():
     fake_db.add.assert_called_once()
     fake_db.commit.assert_called_once()
     fake_db.refresh.assert_called_once()
+    
+def test_get_transactions_success_case():
+    fake_db = MagicMock()
+    
+    fake_query = MagicMock()
+    
+    fake_query.filter_by.return_value = fake_query
+    fake_query.filter.return_value = fake_query
+    fake_query.all.return_value = ['fake_transaction']
+    
+    fake_db.query.return_value = fake_query
+    
+    fake_user = MagicMock()
+    fake_user.id = 1
+    
+    fake_filters = TransactionFilters(type = 'income', category = 'fake_category')
+    
+    result = get_transactions(fake_filters, fake_user, fake_db)
+    
+    assert result[0] == 'fake_transaction'
