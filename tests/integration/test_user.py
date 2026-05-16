6e4b41e90e838b674bed9a9b5e5da82177286d67
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-05-16 08:20:30 -0300

    test: add user creation duplicate email integration test

--- tests/integration/test_user.py
+++ tests/integration/test_user.py
@@ -8,3 +8,11 @@ def test_create_user_success_case():
     
     assert response.status_code == 200
     assert response.json()["email"] == "test_email@test.com"
+    
+def test_create_user_with_email_existing_case():
+    client.post('/user', json = {"email": "email_user_test@test.com", "password": "123"})
+    
+    response = client.post('/user', json = {"email": "email_user_test@test.com", "password": "123"})
+    
+    assert response.status_code == 409
+    assert response.json()["detail"] == 'Email already exists'
