675b9ecd61e953e261740e8f33f49dbbedd297bc
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-05-18 20:38:30 -0300

    test: add get transaction success case

--- tests/integration/test_transaction.py
+++ tests/integration/test_transaction.py
@@ -16,3 +16,18 @@ def test_create_transaction_success_case():
     assert response.json()['amount'] == 500
     assert response.json()['category'] == 'fake category'
     assert response.json()['description'] == 'fake description'
+    
+def test_get_transaction_success_case():
+    email = create_user_with_fake_email()
+    
+    access_token = get_access_token(email, '123')
+    
+    client.post('/transaction', json = {"type": "income", "amount": 500, "category": "fake category", "description": "fake description"}, headers = {"Authorization": f'Bearer {access_token}'})
+    
+    response = client.get('/transaction', params = {"type": "income"}, headers = {"Authorization": f'Bearer {access_token}'})
+    
+    assert response.status_code == 200
+    assert response.json()[-1]['type'] == 'income'
+    assert response.json()[-1]['amount'] == 500
+    assert response.json()[-1]['category'] == 'fake category'
+    assert response.json()[-1]['description'] == 'fake description'
