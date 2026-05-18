b30b1686cc2868aa39b51690a39b5782743f2cb1
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-05-18 20:43:11 -0300

    test: fix user update email collision with faker

--- tests/integration/test_user.py
+++ tests/integration/test_user.py
@@ -11,10 +11,12 @@ client = TestClient(app)
 app.dependency_overrides[get_db] = override_get_db
 
 def test_create_user_success_case():
-    response = client.post('/user', json = {"email": "test_email@test.com", "password": "123"})
+    fake_email = faker.email()
+    
+    response = client.post('/user', json = {"email": fake_email, "password": "123"})
     
     assert response.status_code == 200
-    assert response.json()["email"] == "test_email@test.com"
+    assert response.json()["email"] == fake_email
     
 def test_create_user_with_email_existing_case():
     client.post('/user', json = {"email": "email_user_test@test.com", "password": "123"})
@@ -47,10 +49,12 @@ def test_update_me_success_case():
     
     access_token = get_access_token(email, "123")
     
-    response = client.patch('/me', json = {"email": "new_email@test.com"}, headers = {"Authorization": f'Bearer {access_token}'})
+    new_fake_email = faker.email()
+    
+    response = client.patch('/me', json = {"email": new_fake_email}, headers = {"Authorization": f'Bearer {access_token}'})
     
     assert response.status_code == 200
-    assert response.json()["email"] == "new_email@test.com"
+    assert response.json()["email"] == new_fake_email
     
 def test_update_me_email_already_existing_case():   
     email = create_user_with_fake_email()
