0ed5b25e00403828c94bad40e75fae583ee337d9
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-05-29 06:45:19 -0300

    feat: add logging to user services

--- services/users.py
+++ services/users.py
@@ -3,6 +3,7 @@ from schemas.users import UserCreate, UserLogin, TokenResponse, UpdateUser, Upda
 from models.users import User
 from core.security import hash_password, verify_password, create_access_token
 from fastapi import HTTPException
+from loguru import logger
 
 def create_user(user: UserCreate, db: Session):
     password_hashed = hash_password(user.password)
@@ -10,6 +11,8 @@ def create_user(user: UserCreate, db: Session):
     email_exists = db.query(User).filter_by(email = user.email).first()
     
     if email_exists:
+        logger.warning(f'Creating user failed. email {user.email} already exists')
+        
         raise HTTPException(status_code=409, detail='Email already exists')
     
     new_user = User(email = user.email, password_hash = password_hashed)
@@ -20,18 +23,26 @@ def create_user(user: UserCreate, db: Session):
     
     db.refresh(new_user)
     
+    logger.info(f'User created. Id user: {new_user.id}')
+    
     return new_user
 
 def login_user(user_data: UserLogin, db: Session):
     user = db.query(User).filter_by(email = user_data.email).first()
     
     if not user:
+        logger.warning(f'Login failed. Email {user_data.email} not found.')
+        
         raise HTTPException(status_code=404, detail='User not exists! ') 
     
     if not verify_password(user_data.password, user.password_hash):
+        logger.warning(f'Login failed. Invalid credentials for user {user.id}')
+        
         raise HTTPException(status_code=401, detail='Invalid credentials! ')
     
     access_token = create_access_token({'sub': str(user.id)})
+    
+    logger.info(f'Access token created for {user.id}')
     
     return TokenResponse(access_token=access_token, token_type='bearer')
 
@@ -40,6 +51,8 @@ def update_me(new_data: UpdateUser, user: User, db: Session):
         new_email_exists = db.query(User).filter_by(email = new_data.email).first()
     
         if new_email_exists and new_email_exists.id != user.id:
+            logger.warning(f'Update user failed. Email {new_data.email} already used')
+            
             raise HTTPException(status_code=409, detail='Email already exists')
         
     new_data_dict = new_data.model_dump(exclude_unset=True)
@@ -51,6 +64,8 @@ def update_me(new_data: UpdateUser, user: User, db: Session):
     
     db.refresh(user)
     
+    logger.info(f'User {user.id} updated')
+    
     return user
 
 def update_password(new_password: UpdatePassword, user: User, db: Session):
@@ -59,5 +74,7 @@ def update_password(new_password: UpdatePassword, user: User, db: Session):
     db.commit()
     
     db.refresh(user)
+    
+    logger.info(f'Password updated by user {user.id}')
     
     return user
