9d5344623a56e138d5dd9f03336d71603e41ae79
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-06-05 22:03:39 -0300

    feat: add request id middleware

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from routers.users import router as users_router
 from routers.transactions import router as transaction_router
 from models.users import User 
@@ -6,6 +6,7 @@ from models.transactions import Transaction
 from database import Base, engine
 from contextlib import asynccontextmanager
 import logger_config
+import uuid
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -14,6 +15,15 @@ async def lifespan(app: FastAPI):
     yield
     
 app = FastAPI(lifespan=lifespan)
+
+@app.middleware("http")
+async def add_request_id(request: Request, call_next):
+
+    request.state.request_id = str(uuid.uuid4())
+
+    response = await call_next(request)
+
+    return response
 
 app.include_router(transaction_router)
 app.include_router(users_router)
