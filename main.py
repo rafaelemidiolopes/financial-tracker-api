3e16ab9471f1df7284bf325a21f762a8fd61cc86
rafaelemidiolopes <121732368+rafaelemidiolopes@users.noreply.github.com>
2026-06-07 16:19:56 -0300

    feat: implement request logging and expose X-Request-ID header

--- main.py
+++ main.py
@@ -19,10 +19,19 @@ app = FastAPI(lifespan=lifespan)
 @app.middleware("http")
 async def add_request_id(request: Request, call_next):
 
-    request.state.request_id = str(uuid.uuid4())
+    request_id = str(uuid.uuid4())
+    
+    request.state.request_id = request_id
 
-    response = await call_next(request)
-
+    with logger_config.logger.contextualize(request_id = request_id):
+        logger_config.logger.info(f'Request started. Request method: {request.method}. Request url: {request.url.path}.')
+    
+        response = await call_next(request)
+        
+        response.headers["X-Request-ID"] = request_id
+        
+        logger_config.logger.info(f'Request finished. Request method: {request.method}. Request url: {request.url.path}. Status code: {response.status_code}')
+        
     return response
 
 app.include_router(transaction_router)
